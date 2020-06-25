eec7c623e4b2c4856e7f2a70fd5d205a9f9c7e09
tranthuy0696 <tranthuy0696@gmail.com>
2020-06-25 10:02:05 -0700

    update

---
+++ assets/js/toggle-sub-menu.js
@@ -0,0 +1,21 @@
+$("document").ready(function(){
+      //neo footer xuống bottom nếu nội dung file ngắn hơn cửa sổ
+      if ($(document).height() > ($('footer').offset().top + $('footer').height())) {
+        $('footer').addClass('fixed');
+    }
+    $('.menu__all-categories').on("mouseenter", function(){
+        $('.sub-categories').slideDown("slow");
+    });
+    $('.menu__all-categories').on("mouseleave", function() {
+        if (!$('.sub-categories').is(':hover')) { 
+            $('.sub-categories').slideUp("slow");
+        }
+
+    })
+    $('.sub-categories').on("mouseleave", function() {
+        if (!$('.menu__all-categories').is(':hover')) { 
+            $('.sub-categories').slideUp("slow");
+        }
+
+    })
+})
